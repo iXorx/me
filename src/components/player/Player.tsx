0350a85ae4b867753b57a57fd61ea4a040322d3a
Jordi Turull <jordi.frontend@gmail.com>
2025-02-02 02:13:29 +0100

    add videos

--- src/components/player/Player.tsx
+++ src/components/player/Player.tsx
@@ -16,40 +16,58 @@ import {
 } from '@vidstack/react/player/layouts/default'
 import '@vidstack/react/player/styles/default/layouts/video.css'
 import '@vidstack/react/player/styles/default/theme.css'
-import { useRef } from 'react'
+import { useRef, useState } from 'react'
 
 export function Player() {
 	const player = useRef<MediaPlayerInstance>(null)
+	const [isAudioDescription, setIsAudioDescription] = useState<boolean>(false)
 
 	return (
 		<>
 			<MediaPlayer
 				className={styles.player}
 				title='Presentación Jordi Turull'
-				src={'https://files.vidstack.io/sprite-fight/720p.mp4'}
+				src={isAudioDescription ? '/video/audiodesc.mp4' : '/video/cv.mp4'}
 				crossOrigin
-				playsInline
 				ref={player}
 			>
 				<MediaProvider>
 					<Poster
 						className='vds-poster'
-						src='https://files.vidstack.io/sprite-fight/poster.webp'
-						alt='Girl walks into campfire with gnomes surrounding her friend ready for their next meal!'
+						src='/video/caratula.png'
+						alt='Curriculum Vitae Jordi Turull'
 					/>
 					{textTracks.map((track) => (
 						<Track {...track} key={track.src} />
 					))}
 				</MediaProvider>
 
-				{/* Layouts */}
 				<DefaultAudioLayout icons={defaultLayoutIcons} />
 				<DefaultVideoLayout
 					icons={defaultLayoutIcons}
-					thumbnails='https://files.vidstack.io/sprite-fight/thumbnails.vtt'
 				/>
 			</MediaPlayer>
+			<button onClick={() => setIsAudioDescription(!isAudioDescription)} aria-pressed={isAudioDescription} className={styles.button}>
+				Ver video {isAudioDescription ? 'sin' : 'con'} audiodescripción
+			</button>
 			<p>Presentación de Jordi Turull para la asignatura del master.</p>
+			<ul aria-label='Entrega de la asignatura'>
+				<li>
+					<a href="/video/cv.mp4">
+						Video sin audiodescripción
+					</a>
+				</li>
+				<li>
+					<a href="/video/audiodesc.mp4">
+						Video con audiodescripción
+					</a>
+				</li>
+				<li>
+					<a href="/video/cv.vtt">
+						Subtítulos en español
+					</a>
+				</li>
+			</ul>
 		</>
 	)
 }
--- src/components/player/tracks.ts
+++ src/components/player/tracks.ts
@@ -1,23 +1,10 @@
 export const textTracks = [
 	// Subtitles
 	{
-		src: 'https://files.vidstack.io/sprite-fight/subs/english.vtt',
-		label: 'English',
-		language: 'en-US',
-		kind: 'subtitles',
-		default: true,
-	},
-	{
-		src: 'https://files.vidstack.io/sprite-fight/subs/spanish.vtt',
-		label: 'Spanish',
+		src: '/video/cv.vtt',
+		label: 'Español (ES)',
 		language: 'es-ES',
 		kind: 'subtitles',
-	},
-	// Chapters
-	{
-		src: 'https://files.vidstack.io/sprite-fight/chapters.vtt',
-		kind: 'chapters',
-		language: 'en-US',
 		default: true,
 	},
 ] as const
