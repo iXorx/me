e4c7e6c3cdffc921cbfa1632594b4d5c50cbdeaf
Jordi Turull <jordi.frontend@gmail.com>
2026-05-21 14:27:26 +0200

    update deps and improvement

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
 	output: "export",
-	assetPrefix: process.env.NODE_ENV === "production" ? "/me/" : "",
+	assetPrefix: process.env.NODE_ENV === "production" ? "/me" : "",
 	basePath: process.env.NODE_ENV === "production" ? "/me" : "",
 	trailingSlash: false,
 	images: {
